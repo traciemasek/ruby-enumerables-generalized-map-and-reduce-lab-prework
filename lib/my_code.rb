67e631994b05a0ab0dc5d7ce0ca6d9eddad88d04
Tracie Masek <traciemasek@gmail.com>
2019-06-22 17:11:39 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+def map(arr)
+  new = []
+  i = 0
+  while i < arr.length do
+    new << yield(arr[i])
+    i += 1
+  end
+  new
+end
+
+#i straight up do not understand reduce and had to look at the solution, need to review the idea of the accumulator
+def reduce(arr, start=nil)
+  if start
+    acc = start 
+    i = 0 
+  else
+    acc = arr[0]
+    i = 1
+  end
+  while i < arr.length do
+    acc = yield(acc, arr[i])
+    i +=1 
+  end
+  acc
+end
