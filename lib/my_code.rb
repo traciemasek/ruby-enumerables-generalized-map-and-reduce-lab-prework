1edbf109e5491b1776c1e1857915c7a5154d62a1
Tracie Masek <traciemasek@gmail.com>
2019-06-22 19:55:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,12 +3,17 @@ def map(arr)
   new = []
   i = 0
   while i < arr.length do
+<<<<<<< HEAD
     new << yield(arr[i])
+=======
+    new << yield(arr)
+>>>>>>> f743fbab2116d3afd133633d1bb0691cb3290ad1
     i += 1
   end
   new
 end
 
+<<<<<<< HEAD
 #i straight up do not understand reduce and had to look at the solution, need to review the idea of the accumulator
 def reduce(arr, start=nil)
   if start
@@ -23,4 +28,8 @@ def reduce(arr, start=nil)
     i +=1 
   end
   acc
+=======
+def reduce(arr, start=nil)
+  
+>>>>>>> f743fbab2116d3afd133633d1bb0691cb3290ad1
 end
